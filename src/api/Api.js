696c48e80a87b167248eb0e88c79c5c6a06fe41e
BewareShips <49000772+BewareShips@users.noreply.github.com>
2020-11-06 14:01:41 +0300

    Update Api.js

--- src/api/Api.js
+++ src/api/Api.js
@@ -2,13 +2,12 @@ import Axios from "axios";
 
 
 const api = {
-  key: "30dbd0628bbb7fb3d48a3503d344bee7",
   url: "http://api.openweathermap.org/data/2.5/",
 };
 
 
 
 export const sendQuery = (city) => {
-   return Axios.get(`${api.url}weather?q=${city}&lang=ru&appid=${api.key}`).then(res=>res.data)
+   return Axios.get(`${api.url}weather?q=${city}&lang=ru&appid=30dbd0628bbb7fb3d48a3503d344bee7`).then(res=>res.data)
 };
 
